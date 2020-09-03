e1f35b021a2571a26556374bf1d2871e22218403
igorhristov <igorhristov@sourcemxacademy.mk>
2020-09-03 17:20:33 +0200

    movie react app and dependecies and first componet

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './components/elements/Header';
+const App = () => {
+    return (
+        <div>
+            <Header />i am app
+        </div>
+    );
+};
+
+export default App;
