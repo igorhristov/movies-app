697c7929133f87b1a99f84f5870b4dd38f4778c3
igorhristov <igorhristov@sourcemxacademy.mk>
2020-09-03 21:49:51 +0200

    Styled Components Global Styling and Spiner Styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
 import Header from './components/elements/Header';
 import Home from './components/Home';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`;
+
 const App = () => {
     return (
         <>
             <Header />
             <Home />
+            <GlobalStyle />
         </>
     );
 };
