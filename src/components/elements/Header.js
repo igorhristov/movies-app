e2d5fadfa428ebdd1c641617dbb00610ed971436
igorhristov <igorhristov@sourcemxacademy.mk>
2020-09-03 19:19:50 +0200

    Styled Components Header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import RMDBLogo from '../../images/THEMOVIEDB.png';
+import TMDBLogo from '../../images/TMDB.svg';
+
+const StyledHeader = styled.div`
+    background: #1c1c1c;
+    padding: 0 20px;
+    box-sizing: border-box;
+
+    .header-content {
+        max-width: 1280px;
+        min-height: 120px;
+        padding: 20px 0px;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
+
+    @media screen and (max-width: 500px) {
+        min-height: 0px;
+    }
+`;
+
+const StyledRMDBLogo = styled.img`
+    width: 250px;
+    margin-top: 20px;
+
+    @media screen and (max-width: 500px) {
+        width: 150px;
+        margin-top: 5px;
+    }
+`;
+
+const StyledTMDBLogo = styled.img`
+    width: 122px;
+    margin-top: 25px;
+    float: right;
+
+    @media screen and (max-width: 500px) {
+        display: inline-block;
+        with: 80px;
+        margin-top: 0px;
+    }
+`;
 
 const Header = (props) => {
-    return <div>Header</div>;
+    return (
+        <StyledHeader>
+            <div className='header-content'>
+                <StyledRMDBLogo src={RMDBLogo} alt='rmdb-logo' />
+                <StyledTMDBLogo src={TMDBLogo} alt='tmdb-logo' />
+            </div>
+        </StyledHeader>
+    );
 };
-
-Header.propTypes = {};
 
 export default Header;
