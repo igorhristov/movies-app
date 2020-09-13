24a8c12df93a8ba5b02a9bfcc88b23095f8eef46
igorhristov <igorhristov@sourcemxacademy.mk>
2020-09-14 00:49:16 +0200

    Scaffolding Movie Page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const Movie = ({ movieId }) => {
-    return <div>Movie: {movieId}</div>;
-};
+import Navigation from './elements/Navigation';
+import MovieInfo from './elements/MovieInfo';
+import MovieInfoBar from './elements/MovieInfoBar';
+import Actor from './elements/Actor';
+import Grid from './elements/Grid';
+import Spinner from './elements/Spinner';
 
+const Movie = ({ movieId }) => (
+    <>
+        <Navigation />
+        <MovieInfo />
+        <MovieInfoBar />
+        <Grid>
+            <Actor />
+        </Grid>
+        <Spinner />
+    </>
+);
 export default Movie;
---
+++ src/components/elements/MovieInfoBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovieInfoBar = () => {
+    return <div>Movie Info Bar</div>;
+};
+
+export default MovieInfoBar;
